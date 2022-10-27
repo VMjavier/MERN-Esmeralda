6bbf263d56ff57cba6d67ee521123b4eeb0b87fb
JavierV <110652153+VMjavier@users.noreply.github.com>
2022-10-27 16:27:15 -0500

    Avance-01-React-app

--- amazon/frontend/src/App.js
+++ amazon/frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <header>
-        <a href="/">Esmeralda</a>
+        <a href="/">Joyeria Esmeralda</a>
       </header>
       <main>lista de productos</main>
     </div>
