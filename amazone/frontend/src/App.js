cfc4bb0ba16f20ae4284b8c2f6dd5b66149fe56e
JavierV <110652153+VMjavier@users.noreply.github.com>
2022-10-29 10:54:41 -0500

    Version-02-React-App

--- amazone/frontend/src/App.js
+++ amazone/frontend/src/App.js
@@ -1,11 +1,34 @@
+import React from 'react';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen.js';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Joyeria Esmeralda</a>
-      </header>
-      <main>lista de productos</main>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Esmeralda
+            </Link>
+          </div>
+          <div>
+            <a href="/cart">Productos</a>
+            <a href="/signin">Carrito</a>
+            <a href="/cart">Ingresa</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+        <footer className="row center">Esmeralda S.A.S</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
