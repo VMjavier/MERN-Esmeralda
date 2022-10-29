2af0f06f0fb51cb6d9ccae8289004db07f6b4095
JavierV <110652153+VMjavier@users.noreply.github.com>
2022-10-29 11:34:06 -0500

    Avance-03-React-App

--- amazone/frontend/src/App.js
+++ amazone/frontend/src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
-
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen.js';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <Link className="brand" to="/">
-              Esmeralda
-            </Link>
-          </div>
-          <div>
-            <a href="/cart">Productos</a>
-            <a href="/signin">Carrito</a>
-            <a href="/cart">Ingresa</a>
-          </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="dark">
+                <Navbar.Brand>Esmeralda</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
         <footer className="row center">Esmeralda S.A.S</footer>
       </div>
